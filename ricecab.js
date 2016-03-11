4f6e236a2c70497a2c28d62ba8cd00741a5fc129
Evan Lin <evan.linjin@gmail.com>
2016-03-12 01:14:32 +1300

    Begin removing dependance on wc.

--- ricecab.js
+++ ricecab.js
@@ -318,9 +318,8 @@ function get_total_cost(path, user_id) {
     // Convert data to lowercase, spaceless string.
     var file_str = data.toString();
     file_str = file_str.replace(/\s+/g, '').toLowerCase();
-    bot.sendMessage(user_id, file_str);
 
-    var cmp_str = 'cost';
+    var cmp_str = 'cost:';
     var total_cost = 0.00;
 
     for (var i = 0; i < file_str.length; i++) {
