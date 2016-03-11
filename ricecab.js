2f7bdf39b2c40798603234d968223d7c38ce916d
Evan Lin <evan.linjin@gmail.com>
2016-03-12 01:56:05 +1300

    Begin removing dependance on wc.

--- ricecab.js
+++ ricecab.js
@@ -44,9 +44,9 @@ bot.onText(/\/checkin/, function(msg, match) {
     // CHECKIN REJECTION >>
     if (chatId !== userId) {
         reject_msg =
-        "To avoid mess in the chatgroup, please add and message the /checkin command to me personally. " +
+        "To avoid mess in the chatgroup, please add me; @ricecab_bot and message the /checkin command to me personally." +
         "If you have no idea what I am talking about, please ask Evan. \n\n" +
-        "Your '/checkin' request was not recorded.";
+        "WARNING: YOUR '/checkin' REQUEST WAS NOT RECORDED.";
         bot.sendMessage(chatId, reject_msg); return;
     }
 
